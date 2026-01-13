b5ce77f285510b3514ee0c0f3f70e86a2911a63d
cezarymilek <118814926+czarekmilek@users.noreply.github.com>
2026-01-13 13:26:01 +0100

    Routing fix

--- otterly-well/src/main.tsx
+++ otterly-well/src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider.tsx";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename={"/OtterlyWell"}>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
