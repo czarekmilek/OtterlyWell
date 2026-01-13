ba677691ad840e8489b91d73dfd440ac300bbbb0
cezarymilek <118814926+czarekmilek@users.noreply.github.com>
2026-01-13 13:07:00 +0100

    Updates

--- assets/InputField-NGRfTM-q.js
+++ assets/InputField-NGRfTM-q.js
@@ -1,4 +1,4 @@
-import{j as r}from"./index-BseDcRp9.js";import{m as i}from"./proxy-CBzavYgd.js";const d=({type:t,placeholder:e,value:n,onChange:o,icon:a})=>r.jsxs("div",{className:"relative group",children:[r.jsx("div",{className:`absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none \r
+import{j as r}from"./index-B2Fph0LG.js";import{m as i}from"./proxy-j0g64NeB.js";const d=({type:t,placeholder:e,value:n,onChange:o,icon:a})=>r.jsxs("div",{className:"relative group",children:[r.jsx("div",{className:`absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none \r
                   text-brand-neutral-dark/50 group-focus-within:text-brand-primary \r
                   transition-colors duration-300`,children:r.jsx(a,{className:"text-xl",hidden:n})}),r.jsx(i.input,{whileFocus:{scale:1.01},transition:{duration:.2},type:t,value:n,onChange:o,placeholder:e,className:`block w-full pl-10 pr-3 py-3 border-none rounded-xl bg-white/50 backdrop-blur-lg \r
               text-brand-neutral-dark placeholder-brand-neutral-dark/50 shadow-sm ring-1 \r
