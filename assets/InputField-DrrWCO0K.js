aaf14fed0ac53beb83cb1ed4268cf1919ce093f2
cezarymilek <118814926+czarekmilek@users.noreply.github.com>
2026-01-22 18:51:11 +0100

    Updates

--- assets/InputField-DrrWCO0K.js
+++
@@ -1,6 +0,0 @@
-import{j as r}from"./index-1dX7M82B.js";import{m as i}from"./proxy-IKh-44iE.js";const d=({type:t,placeholder:e,value:n,onChange:o,icon:a})=>r.jsxs("div",{className:"relative group",children:[r.jsx("div",{className:`absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none \r
-                  text-brand-neutral-dark/50 group-focus-within:text-brand-primary \r
-                  transition-colors duration-300`,children:r.jsx(a,{className:"text-xl",hidden:n})}),r.jsx(i.input,{whileFocus:{scale:1.01},transition:{duration:.2},type:t,value:n,onChange:o,placeholder:e,className:`block w-full pl-10 pr-3 py-3 border-none rounded-xl bg-white/50 backdrop-blur-lg \r
-              text-brand-neutral-dark placeholder-brand-neutral-dark/50 shadow-sm ring-1 \r
-              ring-brand-depth focus:ring-2 focus:ring-brand-primary focus:outline-none \r
-              focus:bg-brand-neutral-light transition-colors duration-300`})]});export{d as I};
